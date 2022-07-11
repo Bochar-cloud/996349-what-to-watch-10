c45f02bd488a05080c8e3ed8cd0ecc1d929cb424
Sergei Bocharov <bocharov@texterra.ru>
2022-07-11 14:44:20 +0300

    Начинаем программировать

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,31 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/main/main';
+
+type AppScreenProps = {
+  numbersMovies: number,
+  nameMovie: string,
+  genreMovie: string,
+  reliseMovie: Date,
+  promoMovie: string,
+}
+
+function App (props:AppScreenProps): JSX.Element {
+  const {
+    numbersMovies,
+    nameMovie,
+    genreMovie,
+    reliseMovie,
+    promoMovie,
+  } = props;
+
+  return (
+    <MainScreen
+      numbersMovies = {numbersMovies}
+      nameMovie = {nameMovie}
+      genreMovie = {genreMovie}
+      reliseMovie = {reliseMovie}
+      promoMovie = {promoMovie}
+    />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Settings = {
+  NUMBERS_MOVIES: 20,
+  NAME_MOVIE: 'The Grand Budapest Hotel',
+  GENRE_MOVIE: 'Drama',
+  RELISE_MOVIE: new Date(),
+  PROMO_MOVIE: 'img/the-grand-budapest-hotel-poster.jpg',
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      numbersMovies = {Settings.NUMBERS_MOVIES}
+      nameMovie = {Settings.NAME_MOVIE}
+      genreMovie = {Settings.GENRE_MOVIE}
+      reliseMovie = {Settings.RELISE_MOVIE}
+      promoMovie = {Settings.PROMO_MOVIE}
+    />
   </React.StrictMode>,
 );
